8f295462ab392ac9e95f7f2d298f7c29570218ac
Adil <sarsembin.adil29@gmail.com>
2022-12-12 17:46:08 +0600

    auth

--- django_project/settings.py
+++ django_project/settings.py
@@ -45,8 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
-    'api',
+    'djoser',
     'corsheaders',
+
+    'api',
     'userauth',
 ]
 
@@ -101,6 +103,14 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sarsembin.adil29@gmail.com'
+EMAIL_HOST_PASSWORD = 'eljynkugehzhhbmr'
+EMAIL_USE_TLS = True
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -143,3 +153,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'userauth.User'
--- userauth/models.py
+++ userauth/models.py
@@ -1,7 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 # Create your models here.
 
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, name, surname, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
 
-class User(models.Model):
-    pass
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, surname=surname)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'surname']
+
+    def get_name(self):
+        return self.name
+
+    def get_surname(self):
+        return self.surname
+
+    def __repr__(self):
+        return self.email
